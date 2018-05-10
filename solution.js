162778e48b72818b199f9f1771f6e841bb71b913
CK <kok.chee.kean@outlook.com>
2018-05-11 00:21:16 +0800

    additional solution using pseudo-pointers (just the array index really)

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
 let phonebook = require('./phonebook.json');
-
+let timed = true;
+let then = null;
+let now = null;
 let numIteration = 0;
 
 function binarySearchActual(toSearch, searchValue) {
@@ -16,10 +18,36 @@ function binarySearchActual(toSearch, searchValue) {
     return result;
 };
 
+function binarySearchPseudoPointer(toSearch, searchValue) {
+    let start = 0, end = toSearch.length - 1, index = Math.floor((start + end) * 0.5);
+    numIteration++;
+    while (toSearch[index] !== searchValue && start !== end) {
+        numIteration++;
+        toSearch[index] < searchValue ? start = index : end = index;
+        index = Math.floor((start + end) * 0.5);
+        if (start === index) {
+            index = end;
+            break;
+        }
+    };
+    if (timed === true) {
+        now = new Date().getTime();
+        console.log("Run time:", now - then, "s");
+    };
+    return toSearch[index] === searchValue;
+};
+
 function binarySearch(nameList, searchValue) {
     numIteration = 0;
     let toSearch = nameList.sort();
-    return binarySearchActual(toSearch, searchValue);
+    if (timed === true) {then = new Date().getTime()};
+    // let result = binarySearchActual(toSearch, searchValue);
+    let result = binarySearchPseudoPointer(toSearch, searchValue);
+    if (timed === true) {
+        now = new Date().getTime();
+        console.log("Run time:", now - then, "s");
+    };
+    return result;
 };
 
 const nameList = [
@@ -56,11 +84,67 @@ function advBinarySearchActual(toSearch, attr, val) {
     return result;
 }
 
+function advBinarySearchPseudoPointer(toSearch, attr, val) {
+    let start = 0, end = toSearch.length - 1, index = Math.floor((start + end) * 0.5);
+    numIteration++;
+    while (toSearch[index][attr] !== val && start !== end) {
+        numIteration++;
+        toSearch[index][attr] < val ? start = index : end = index;
+        index = Math.floor((start + end) * 0.5);
+        if (start === index) {
+            index = end;
+            break;
+        }
+    };
+    return toSearch[index][attr] === val;
+};
+
 function advBinarySearch(phonebook, attribute, value) {
     numIteration = 0;
     let toSearch = sortByAttribute(phonebook, attribute);
-    return advBinarySearchActual(toSearch, attribute, value);
+    if (timed === true) {then = new Date().getTime()};
+    let result = advBinarySearchActual(toSearch, attribute, value);
+    // let result = advBinarySearchPseudoPointer(toSearch, attribute, value);
+    if (timed === true) {
+        now = new Date().getTime();
+        console.log("Run time:", now - then, "s");
+    };
+    return result;
+}
+
+// == Artificially create a delay with a promise ==
+function delay(x, then) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{resolve(`Slept for ${(new Date().getTime()) - then}ms.`)}, x)
+    })
+}
+
+async function sleep(x, log = false) {
+    let result = await delay(x, new Date().getTime());
+    log === true ? console.log(result) : result;
+}
+
+// == For benchmarking (synchronous) functions ==
+function benchmark(func) {
+    let start = new Promise((resolve, reject) => {
+        let then = new Date().getTime();
+        let result = func();
+        resolve(new Date().getTime() - then);
+    });
+    start.then((time) => {console.log("Ran for", time, "ms.")})
 }
 
 console.log(advBinarySearch(phonebook, "name", "Carleton"), numIteration);
-console.log(advBinarySearch(phonebook, "name", "Ty"), numIteration);
+console.log(advBinarySearch(phonebook, "name", "abcde"), numIteration);
+
+// function wrapper() {
+//     let i = 10000000;
+//     let str = "abcde";
+//     while (i > 0) {
+//         str = str+"abdce";
+//         i--;
+//     }
+//     return str;
+// }
+
+// benchmark(wrapper);
